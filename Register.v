592c7387613d0d235b4243c683c064ea21a5d077
Jean-Kenned <jeanjk99@hotmail.com>
2017-03-19 20:15:56 -0300

    Add files via upload

--- Register.v
+++ Register.v
@@ -1,30 +1,11 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    13:11:33 03/14/2017 
-// Design Name: 
-// Module Name:    Register 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module Register(readReg1,readReg2,writeRegister,clk,writeData,readData1,readData2,regWrite);
+
+module Register(readReg1,readReg2,writeRegister,writeData,readData1,readData2,regWrite);
             input[4:0]readReg1;
 				input [4:0]readReg2;
 				input [4:0]writeRegister;
             input [31:0]writeData;
 				input regWrite;
-				input clk;
 				wire [31:0]readData1;
 				wire [31:0]readData2;
 				output [31:0]readData1;
@@ -42,16 +23,15 @@ module Register(readReg1,readReg2,writeRegister,clk,writeData,readData1,readData
 						    RegMemory[i][j]=1'b0;
 						end
 				  
-				  end
+				  end 
 				end
 				
 				assign  readData1 = RegMemory[readReg1];
-			   assign  readData2 = RegMemory[readReg2];     
-			   always @ (negedge clk)
-            begin
+			   assign  readData2 = RegMemory[readReg2];   
+				
+		      initial begin 
 				    if(regWrite==1'b1)
-					 begin
 					    RegMemory[writeRegister]=writeData;
-					 end
-				end
+			   end
+			  
 endmodule
