e6ab289bc491707dd738caf49fdc48d8845d1548
omelete27 <phmbbatista@gmail.com>
2017-03-19 23:11:10 -0300

    Update PC.v

--- PC.v
+++ PC.v
@@ -19,15 +19,15 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module PC(in,out,clk);
-    input  [7:0]in;
-	 input clk;
-	 output [7:0]out;
+	input  [7:0]in; // sinal de entrada de 8 bits
+	 input clk; // sinal de clock
+	output [7:0]out; // sinal de saida de 8 bits
     reg [7:0]out;
 	 initial begin
-	      out = {8{1'b0}};
+		 out = {8{1'b0}}; // o sinal de saida recebe 8 bits 'zero' (00000000)
 	 end
 	 always @ (negedge clk)
 	 begin
-	      out = in;
+	      out = in; // a cada passagem do clock o sinal de saida recebe o sinal de entrada
 	 end
 endmodule
